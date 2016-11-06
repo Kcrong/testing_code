ff83d76fd47f819a11862bee4d692f58a583afc4
kcrong <kcrong@kim82536.pe.kr>
2016-11-06 18:54:03 +0900

    Add image drawing at sample2

--- sample2-reportlab.py
+++ sample2-reportlab.py
@@ -1,5 +1,18 @@
-from reportlab.pdfgen import canvas
+"""
+모듈 의존성을 제외하면 제일 좋은 방법인듯
+폰트를 자동으로 포함시켜줌
+"""
 
-c = canvas.Canvas("hello.pdf")
-c.drawString(100, 750, "Welcome to Reportlab!")
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
+filename = 'test_png.png'
+
+pdfmetrics.registerFont(TTFont('test', 'testfont.ttf'))
+
+c = canvas.Canvas("sample2_result.pdf")
+c.setFont('test', 13)
+# x, y format
+c.drawString(100, 750, "Lorem ipsum dolor sit amet, consectetuer adipiscing elit")
+c.drawImage(filename, 150, 400)
 c.save()
