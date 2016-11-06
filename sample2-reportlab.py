776bd710045251ed8297c752dac621ba32bc6298
kcrong <kcrong@kim82536.pe.kr>
2016-11-06 19:50:59 +0900

    Complete

--- sample2-reportlab.py
+++ sample2-reportlab.py
@@ -2,7 +2,8 @@
 모듈 의존성을 제외하면 제일 좋은 방법인듯
 폰트를 자동으로 포함시켜줌
 """
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.colors import snow
+from reportlab.lib.styles import ParagraphStyle
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.platypus import Image
@@ -13,16 +14,26 @@ filename = 'test_png.png'
 
 pdfmetrics.registerFont(TTFont('test', 'testfont.ttf'))
 
-our_style = (ParagraphStyle(name='custom_style',
-                            fontName='test',
-                            fontSize=30,
-                            leading=10.1
-                            )
-             )
+our_style = ParagraphStyle(name='custom_style',
+                           fontName='test',
+                           fontSize=30,
+                           leading=10.1,
+                           textColor=snow
+                           )
 
-doc = SimpleDocTemplate("image.pdf")
+
+def draw_background(canvas, document):
+    canvas.drawImage('background.jpg', x=0, y=0,
+                     # Default A1 size
+                     width=594, height=841)
+
+
+doc = SimpleDocTemplate("sample2_result.pdf")
 parts = [
-    Paragraph("My name is Hyunwoo, kcrong", our_style),
-    Image(filename)
+    Paragraph("Who am i?", our_style),
+    Image(filename, width=400, height=400),
+    Paragraph("I am Hyunwoo", our_style)
+
 ]
-doc.build(parts)
+
+doc.build(parts, onFirstPage=draw_background)
