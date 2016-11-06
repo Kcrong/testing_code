487f5af0f9d8b4b414b17ae2f7c43962891a6fc5
kcrong <kcrong@kim82536.pe.kr>
2016-11-06 19:13:17 +0900

    Change code

--- sample2-reportlab.py
+++ sample2-reportlab.py
@@ -2,17 +2,27 @@
 모듈 의존성을 제외하면 제일 좋은 방법인듯
 폰트를 자동으로 포함시켜줌
 """
-
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
+from reportlab.platypus import Image
+from reportlab.platypus import Paragraph
+from reportlab.platypus import SimpleDocTemplate
+
 filename = 'test_png.png'
 
 pdfmetrics.registerFont(TTFont('test', 'testfont.ttf'))
 
-c = canvas.Canvas("sample2_result.pdf")
-c.setFont('test', 13)
-# x, y format
-c.drawString(100, 750, "Lorem ipsum dolor sit amet, consectetuer adipiscing elit")
-c.drawImage(filename, 150, 400)
-c.save()
+our_style = (ParagraphStyle(name='custom_style',
+                            fontName='test',
+                            fontSize=30,
+                            leading=10.1
+                            )
+             )
+
+doc = SimpleDocTemplate("image.pdf")
+parts = [
+    Paragraph("My name is Hyunwoo, kcrong", our_style),
+    Image(filename)
+]
+doc.build(parts)
